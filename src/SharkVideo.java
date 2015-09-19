d427bebb711d69c682d495988b5312107d6b7123
Lucie-Aimée Kaffee <fuzzle.lii@gmail.com>
2015-09-20 01:44:51 +0200

    Create SharkVideo.java

---
+++ src/SharkVideo.java
@@ -0,0 +1,159 @@
+import net.sharkfw.knowledgeBase.ContextCoordinates;
+import net.sharkfw.knowledgeBase.ContextPoint;
+import net.sharkfw.knowledgeBase.PeerSemanticTag;
+import net.sharkfw.knowledgeBase.SemanticTag;
+import net.sharkfw.knowledgeBase.SpatialSemanticTag;
+import net.sharkfw.knowledgeBase.TimeSemanticTag;
+
+/**
+ * 
+ *
+ */
+public class SharkVideo {
+	private PeerSemanticTag originator;
+	private PeerSemanticTag peer;
+	private PeerSemanticTag remotePeer;
+	private SemanticTag topic;
+	private TimeSemanticTag time;
+	private SpatialSemanticTag location;
+	private ContextCoordinates contextCoordinates;
+	private ContextPoint contextPoint;
+	
+	public SharkVideo(String videoID) {
+		YouTubeKnowledgeBase ytkb = new YouTubeKnowledgeBase();
+		YouTubeAPI api = new YouTubeAPI("AIzaSyBZBT-ij4JblHC_HS5gv7tiJoLpwHlWjY8");	
+		YouTubeVideo.Snippet s = new YouTubeVideo.Snippet();
+		
+		YouTubeVideo video = null;
+		try {
+			video = api.getVideoById(videoID);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		originator = 	ytkb.createYTPeerSemanticTag(video.getChannelId(), "URL", null);
+		peer = 			ytkb.createYTPeerSemanticTag(video.getChannelId(), "URL", null);
+		remotePeer = 	ytkb.createYTPeerSemanticTag(video.getChannelId(), "URL", null);
+		topic = 			ytkb.createYTSemanticTag(video.getDescription(), "URL");
+		time = 			ytkb.createYTTimeSemanticTag(s.getPublishedAt(), 0);
+		location = null;
+		if (video.getLocation() != null){
+			locationVid =	ytkb.createYTSpatialSemanticTag(video.getLocation().getLongitude(), video.getLocation().getLatitude());
+		}	
+		contextCoordinates = ytkb.createContextCoordinates(topic, originator, peer, remotePeer, time, location);			
+		contextPoint = ytkb.createContextPoint(contextCoordinates);
+	}
+
+	/**
+	 * @return PeerSemanticTag originator
+	 */
+	public PeerSemanticTag getOriginator() {
+		return originator;
+	}
+
+	/**
+	 * @param PeerSemanticTag originator
+	 */
+	public void setOriginator(PeerSemanticTag originator) {
+		this.originator = originator;
+	}
+
+	/**
+	 * @return PeerSemanticTag peer
+	 */
+	public PeerSemanticTag getPeer() {
+		return peer;
+	}
+
+	/**
+	 * @param PeerSemanticTag peer
+	 */
+	public void setPeer(PeerSemanticTag peer) {
+		this.peer = peer;
+	}
+
+	/**
+	 * @return PeerSemanticTag remotePeer
+	 */
+	public PeerSemanticTag getRemotePeer() {
+		return remotePeer;
+	}
+
+	/**
+	 * @param PeerSemanticTag remotePeer
+	 */
+	public void setRemotePeer(PeerSemanticTag remotePeer) {
+		this.remotePeer = remotePeer;
+	}
+
+	/**
+	 * @return SemanticTag topic
+	 */
+	public SemanticTag getTopic() {
+		return topic;
+	}
+
+	/**
+	 * @param SemanticTag topic
+	 */
+	public void setTopic(SemanticTag topic) {
+		this.topic = topic;
+	}
+
+	/**
+	 * @return TimeSemanticTag time
+	 */
+	public TimeSemanticTag getTime() {
+		return time;
+	}
+
+	/**
+	 * @param TimeSemanticTag time
+	 */
+	public void setTime(TimeSemanticTag time) {
+		this.time = time;
+	}
+
+	/**
+	 * @return SpatialSemanticTag location
+	 */
+	public SpatialSemanticTag getLocation() {
+		return location;
+	}
+
+	/**
+	 * @param SpatialSemanticTag location
+	 */
+	public void setLocation(SpatialSemanticTag location) {
+		this.location = location;
+	}
+
+	/**
+	 * @return ContextCoordinates contextCoordinates
+	 */
+	public ContextCoordinates getContextCoordinates() {
+		return contextCoordinates;
+	}
+
+	/**
+	 * @param ContextCoordinates contextCoordinates
+	 */
+	public void setContextCoordinates(ContextCoordinates contextCoordinates) {
+		this.contextCoordinates = contextCoordinates;
+	}
+
+	/**
+	 * @return ContextPoint contextPoint
+	 */
+	public ContextPoint getContextPoint() {
+		return contextPoint;
+	}
+
+	/**
+	 * @param ContextPoint contextPoint
+	 */
+	public void setContextPoint(ContextPoint contextPoint) {
+		this.contextPoint = contextPoint;
+	}
+	
+}
