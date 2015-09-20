e8b4036252b91403735a1a7b5ca534d64f222871
fruchtzwergin <c.kritschmar@gmail.com>
2015-09-20 17:01:18 +0200

    added publishedAt() for Playlists and integrated getURL()

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,8 @@ import api.YouTubeVideo;
 
 public class Main {
     public static void main(String[] args)  {
-    	YouTubeAPI api = new YouTubeAPI("AIzaSyBZBT-ij4JblHC_HS5gv7tiJoLpwHlWjY8");
+    	String apiKey = "AIzaSyBZBT-ij4JblHC_HS5gv7tiJoLpwHlWjY8";
+    	YouTubeAPI api = new YouTubeAPI(apiKey);
     	try {    		
     		System.out.println("--------Returning Video Test Data--------");
     		YouTubeVideo video = api.getVideoById("MDIUreSo0gI");
@@ -44,27 +45,31 @@ public class Main {
     		System.out.println(channel.getUploadedVideosPlaylistId());
     		System.out.println(channel.getUrl());
     		
-    		System.out.println("\n\n--------Returning Playlist Test Data (May take a while for large playlists)--------");
-    		YouTubePlaylist playlist = api.getPlaylistById(channel.getFavoritedVideosPlaylistId());
-    		System.out.println(playlist.getTitle());
-    		System.out.println(playlist.getId());
-    		System.out.println(playlist.getChannelId());
-    		System.out.println(playlist.getChannelTitle());
-    		System.out.println(playlist.getDescription());
-    		System.out.println(playlist.getThumbnailUrl());
-    		System.out.println(playlist.getUrl());
-    		int counter = 1;
-    		for (String videoId: playlist.getVideoIds()) {
-    			System.out.println(counter + ": "+ videoId);
-    			counter++;
-    		}
+//    		System.out.println("\n\n--------Returning Playlist Test Data (May take a while for large playlists)--------");
+//    		YouTubePlaylist playlist = api.getPlaylistById(channel.getFavoritedVideosPlaylistId());
+//    		System.out.println(playlist.getTitle());
+//    		System.out.println(playlist.getId());
+//    		System.out.println(playlist.getChannelId());
+//    		System.out.println(playlist.getChannelTitle());
+//    		System.out.println(playlist.getDescription());
+//    		System.out.println(playlist.getThumbnailUrl());
+//    		System.out.println(playlist.getUrl());
+//    		int counter = 1;
+//    		for (String videoId: playlist.getVideoIds()) {
+//    			System.out.println(counter + ": "+ videoId);
+//    			counter++;
+//    		}
     		
-    		//Scanner scanner = new Scanner(System.in);
-    		//System.out.println("Please enter a Video ID:");
-    		//String vidID = scanner.nextLine();
-    	    //SharkVideo sv = new SharkVideo(video); //use MDIUreSo0gI
-    	    //System.out.println(sv.toString());
-    	    //scanner.close();
+    		Scanner scanner = new Scanner(System.in);
+    		System.out.println("Please enter a Video ID:");
+    		String vidID = scanner.nextLine();
+    		YouTubeKnowledgeBase ytkb = new YouTubeKnowledgeBase();
+    		SharkImporter si = new SharkImporter(ytkb, apiKey);
+    		SharkVideo sv = new SharkVideo(video, ytkb);
+    		//use MDIUreSo0gI
+    		sv.importVideo();
+    	    System.out.println(si.importVideo(vidID));
+    	    scanner.close();
     		
 		} catch (Exception e) {
 			e.printStackTrace();
